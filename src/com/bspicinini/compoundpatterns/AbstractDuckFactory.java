430fcc8bd050c5923d9923c583c779c465d6abc5
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2024-07-15 11:49:59 -0300

    third feature

--- src/com/bspicinini/compoundpatterns/AbstractDuckFactory.java
+++
@@ -1,8 +0,0 @@
-package compoundpatterns;
-
-public abstract class AbstractDuckFactory {
-    public abstract Quackable createMallardDuck();
-    public abstract Quackable createRedHeadDuck();
-    public abstract Quackable createDuckCall();
-    public abstract Quackable createRubberDuck();
-}
--- src/com/bspicinini/compoundpatterns/DuckSimulator.java
+++ src/com/bspicinini/compoundpatterns/DuckSimulator.java
@@ -3,50 +3,22 @@ package compoundpatterns;
 public class DuckSimulator {
 
     public static void main(String[] args) {
+        System.out.println("--------------- Duck Simulator ---------------");
         var simulator = new DuckSimulator();
-        var duckFactory = new CountingDuckFactory();
-        simulator.simulate(duckFactory);
+        simulator.simulate();
     }
 
-    void simulate(AbstractDuckFactory duckFactory) {
-        
-        var redHeadDuck = duckFactory.createRedHeadDuck();
-        var duckCall = duckFactory.createDuckCall();
-        var rubberDuck = duckFactory.createRubberDuck();
+    void simulate() {
+        var redHeadDuck = new QuackCounter(new RedHeadDuck()); 
+        var duckCall = new QuackCounter(new DuckCall());
+        var rubberDuck = new QuackCounter(new RubberDuck());
         var gooseDuck = new GooseAdapter(new Goose());
 
-        System.out.println("\nDuck Simulator: With Composite - Flocks");
-        var flockOfDucks = new Flock();
-        flockOfDucks.add(redHeadDuck);
-        flockOfDucks.add(duckCall);
-        flockOfDucks.add(rubberDuck);
-        flockOfDucks.add(gooseDuck);
-        
-        var mallardDuck = duckFactory.createMallardDuck();
-        var mallardTwo = duckFactory.createMallardDuck();
-        var mallardThree = duckFactory.createMallardDuck();
-        var mallardFour = duckFactory.createMallardDuck();
-
-        var flockOfMallards = new Flock();
-        flockOfMallards.add(mallardDuck);
-        flockOfMallards.add(mallardTwo);
-        flockOfMallards.add(mallardThree);
-        flockOfMallards.add(mallardFour);
-
-        flockOfDucks.add(flockOfMallards);
-        
-        var quackologist = new Quackologist();
-        flockOfDucks.registerObserver(quackologist);
-        System.out.println("\nDuck Simulator: Whole Flock Simulation");    
-        
-        simulate(flockOfDucks);
-        System.out.println("\nDuck Simulator: Mallard Flock Simulation");
-        simulate(flockOfMallards);
+        redHeadDuck.quack();
+        duckCall.quack();
+        rubberDuck.quack();    
+        gooseDuck.quack();        
 
         System.out.println("The ducks quacked " + QuackCounter.getQuacks() + " times");
     }
-
-    void simulate(Quackable duck) {
-        duck.quack();
-    }
-}
+} 
